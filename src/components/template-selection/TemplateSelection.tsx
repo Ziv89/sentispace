060259304bd524dd5a799d9ae90a41bb92d82a9a
nick-vi <nicolas.vilvovsky@gmail.com>
2024-03-01 13:22:22 +0200

    eslint prettier fix import statements in TemplateSelection component

--- src/components/template-selection/TemplateSelection.tsx
+++ src/components/template-selection/TemplateSelection.tsx
@@ -1,56 +1,56 @@
-import classes from './TemplateSelection.module.scss';
+import classes from './TemplateSelection.module.scss'
 
-import FullscreenModal from '../shared/FullscreenModal';
-import Button from '../input/button/Button';
-import { db } from '../../data/Database';
-import { useLiveQuery } from 'dexie-react-hooks';
-import { Activity as IActivity } from '../../data/interfaces';
-import Activity from '../activity/Activity';
-import { IndexableType } from 'dexie';
-import { useState } from 'react';
-import { ArrowLeft } from '@phosphor-icons/react';
+import FullscreenModal from '../shared/FullscreenModal'
+import Button from '../input/button/Button'
+import { db } from '../../data/Database'
+import { useLiveQuery } from 'dexie-react-hooks'
+import { Activity as IActivity } from '../../data/interfaces'
+import Activity from '../activity/Activity'
+import { IndexableType } from 'dexie'
+import { useState } from 'react'
+import { ArrowLeft } from '@phosphor-icons/react'
 
 interface TemplateSelectionProps {
-    onClose: () => void;
+  onClose: () => void
 }
 
 const TemplateSelection = ({ onClose }: TemplateSelectionProps) => {
-    const activities =
-        useLiveQuery<IActivity[]>(() =>
-            db.activities.where('isTemplate').equals(1).toArray()
-        ) ?? [];
+  const activities =
+    useLiveQuery<IActivity[]>(() =>
+      db.activities.where('isTemplate').equals(1).toArray(),
+    ) ?? []
 
-    const [selectedActivity, setSelectedActivity] = useState<IndexableType>(0);
+  const [selectedActivity, setSelectedActivity] = useState<IndexableType>(0)
 
-    return (
-        <FullscreenModal className={classes.modal}>
-            <FullscreenModal.Header>
-                <ArrowLeft size={24} onClick={onClose} />
-                <FullscreenModal.Title className={classes.modalTitle}>
-                    Template Selection
-                </FullscreenModal.Title>
-            </FullscreenModal.Header>
-            <div className={classes.activitiesWrapper}>
-                <div className={classes.activitiesContainer}>
-                    {activities.map((activity, index) => (
-                        <Activity
-                            key={index}
-                            {...activity}
-                            isSelected={activity.id === selectedActivity}
-                            onSelectedActivityChange={setSelectedActivity}
-                            templateView
-                            onCloseTemplateModal={onClose}
-                        />
-                    ))}
-                </div>
-            </div>
-            <FullscreenModal.ButtonsPanel>
-                <Button variant="secondary" onClick={onClose} underline>
-                    Go Back
-                </Button>
-            </FullscreenModal.ButtonsPanel>
-        </FullscreenModal>
-    );
-};
+  return (
+    <FullscreenModal className={classes.modal}>
+      <FullscreenModal.Header>
+        <ArrowLeft size={24} onClick={onClose} />
+        <FullscreenModal.Title className={classes.modalTitle}>
+          Template Selection
+        </FullscreenModal.Title>
+      </FullscreenModal.Header>
+      <div className={classes.activitiesWrapper}>
+        <div className={classes.activitiesContainer}>
+          {activities.map((activity) => (
+            <Activity
+              key={activity.id.toString()}
+              {...activity}
+              isSelected={activity.id === selectedActivity}
+              onSelectedActivityChange={setSelectedActivity}
+              templateView
+              onCloseTemplateModal={onClose}
+            />
+          ))}
+        </div>
+      </div>
+      <FullscreenModal.ButtonsPanel>
+        <Button variant="secondary" onClick={onClose} underline>
+          Go Back
+        </Button>
+      </FullscreenModal.ButtonsPanel>
+    </FullscreenModal>
+  )
+}
 
-export default TemplateSelection;
+export default TemplateSelection
