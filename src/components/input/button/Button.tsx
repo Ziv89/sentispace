c7dcb6f3955cfa75b7f8ce5795d8938da4fa80a8
nick-vi <nicolas.vilvovsky@gmail.com>
2024-03-01 13:12:43 +0200

    eslint prettier refactor input components

--- src/components/input/button/Button.tsx
+++ src/components/input/button/Button.tsx
@@ -1,38 +1,39 @@
-import classNames from 'classnames/bind';
+import classNames from 'classnames/bind'
 
-import classes from './Button.module.scss';
-import { ButtonHTMLAttributes } from 'react';
+import classes from './Button.module.scss'
+import { ButtonHTMLAttributes } from 'react'
 
-const cx = classNames.bind(classes);
+const cx = classNames.bind(classes)
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-    variant: 'primary' | 'secondary';
-    children: string;
-    isDangerous?: boolean;
-    underline?: boolean;
-    className?: string;
+  variant: 'primary' | 'secondary'
+  children: string
+  isDangerous?: boolean
+  underline?: boolean
+  className?: string
 }
 
 const Button = ({
-    variant,
-    children,
-    isDangerous,
-    underline,
-    className,
-    ...props
+  variant,
+  children,
+  isDangerous,
+  underline,
+  className,
+  ...props
 }: ButtonProps) => {
-    return (
-        <button
-            className={cx(className, {
-                [variant]: true,
-                isDangerous,
-                underline,
-            })}
-            {...props}
-        >
-            {children}
-        </button>
-    );
-};
+  return (
+    <button
+      className={cx(className, {
+        [variant]: true,
+        isDangerous,
+        underline,
+      })}
+      type="button"
+      {...props}
+    >
+      {children}
+    </button>
+  )
+}
 
-export default Button;
+export default Button
--- src/components/input/time-picker/TimeIncrementButtons.tsx
+++ src/components/input/time-picker/TimeIncrementButtons.tsx
@@ -1,29 +1,30 @@
-import classes from './TimePicker.module.scss';
+import classes from './TimePicker.module.scss'
 
-import { addMinutes } from 'date-fns';
+import { addMinutes } from 'date-fns'
 
 interface TimeIncrementButtonsProps {
-    arrayOfMinutes: number[];
-    date: Date;
-    onDateChange: (date: Date) => void;
+  arrayOfMinutes: number[]
+  date: Date
+  onDateChange: (date: Date) => void
 }
 
 const TimeIncrementButtons = ({
-    arrayOfMinutes,
-    date,
-    onDateChange,
+  arrayOfMinutes,
+  date,
+  onDateChange,
 }: TimeIncrementButtonsProps) => (
-    <div className={classes.incrementButtons}>
-        {arrayOfMinutes.map((minutes, index) => (
-            <button
-                className={classes.incrementButton}
-                onClick={() => onDateChange(addMinutes(date, minutes))}
-                key={index}
-            >
-                +{minutes}m
-            </button>
-        ))}
-    </div>
-);
+  <div className={classes.incrementButtons}>
+    {arrayOfMinutes.map((minutes) => (
+      <button
+        className={classes.incrementButton}
+        onClick={() => onDateChange(addMinutes(date, minutes))}
+        type="button"
+        key={minutes}
+      >
+        +{minutes}m
+      </button>
+    ))}
+  </div>
+)
 
-export default TimeIncrementButtons;
+export default TimeIncrementButtons
