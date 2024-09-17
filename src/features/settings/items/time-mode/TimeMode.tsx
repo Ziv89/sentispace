815762adf77f84bf40c374c2605fc22ed1bb5bc6
Ziv89 <46285020+Ziv89@users.noreply.github.com>
2024-09-17 17:38:21 +0300

    Update TimeMode.tsx
    Fixed TimeMode to use use24HourModeState localstorage for 24 hour use

--- src/features/settings/items/time-mode/TimeMode.tsx
+++ src/features/settings/items/time-mode/TimeMode.tsx
@@ -1,30 +1,13 @@
-import { useState, useEffect } from 'react';
 import SettingsItem from '../../settings-item/SettingsItem';
 import Switch from '../../../../components/input/switch/Switch';
+import { use24HourModeState } from '../../../../utils/hooks/use24HourModeState';
 
-//Added TimeMode component to item settings, will store 24mode in local storage 
 const TimeMode = () => {
-  const [is24HourMode, setIs24HourMode] = useState<boolean>(() => {
-    try {
-      const savedMode = localStorage.getItem('is24HourMode');
-      return savedMode ? JSON.parse(savedMode) : false;
-    } catch (e) {
-      console.error("Error parsing 'is24HourMode' from localStorage", e);
-      return false;
-    }
-  });
-
-  useEffect(() => {
-    localStorage.setItem('is24HourMode', JSON.stringify(is24HourMode));
-  }, [is24HourMode]);
-
-  const handleToggle = () => {
-    setIs24HourMode(prevMode => !prevMode);
-  };
+  const { is24HourMode, toggle24HourMode } = use24HourModeState(); // Using the custom hook
 
   return (
     <SettingsItem label="24h Mode" iconKey="Clock">
-      <Switch checked={is24HourMode} onChange={handleToggle} />
+      <Switch checked={is24HourMode} onChange={toggle24HourMode} />
     </SettingsItem>
   );
 };
