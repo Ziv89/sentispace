00a063ed6599166477de8b4f769457a854ebc94e
Ziv89 <46285020+Ziv89@users.noreply.github.com>
2024-09-23 09:20:51 +0300

    Update TimeMode.tsx
    Removed the use of context

--- src/features/settings/items/time-mode/TimeMode.tsx
+++ src/features/settings/items/time-mode/TimeMode.tsx
@@ -1,12 +1,11 @@
 import SettingsItem from '../../settings-item/SettingsItem';
 import Switch from '../../../../components/input/switch/Switch';
 
-import { useTimeModeContext } from '../../../../data/contexts/TimeModeContext';
-
+import { use24HourModeState } from '../../../../utils/hooks/use24HourModeState'; // Custom hook
 
 
 const TimeMode = () => {
-  const { is24HourMode, toggle24HourMode } = useTimeModeContext(); // Use context
+  const { is24HourMode, toggle24HourMode } = use24HourModeState(); // Use context
 
   return (
     <SettingsItem label="24h Mode" iconKey="Clock">
