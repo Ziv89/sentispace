935d08d4c11228cca6d1db5fec80c1124908bc38
Ziv89 <46285020+Ziv89@users.noreply.github.com>
2024-10-06 14:28:30 +0300

    Update CategorySelectionModal.tsx

--- src/features/categories/category-selection/CategorySelectionModal.tsx
+++ src/features/categories/category-selection/CategorySelectionModal.tsx
@@ -1,121 +1,125 @@
-import classes from './CategorySelect.module.scss'
-
-import Button from '@input/button/Button'
-import TextField from '@input/text-field/TextField'
-import FullscreenModal from '@modals/FullscreenModal'
-import { ArrowLeft } from '@phosphor-icons/react'
-import { IndexableType } from 'dexie'
-import { useContext, useMemo, useState } from 'react'
-import { CategoriesContext, ICategoriesContext } from '../CategoriesContext'
-import CategoryModal from '../category-edit-modal/CategoryModal'
-import CategoryBadge from './category-badge/CategoryBadge'
-import CategoryItem from './CategoryItem'
-import { CategorySelectionFunction } from './CategorySelect'
+import Button from '@input/button/Button';
+import TextField from '@input/text-field/TextField';
+import FullscreenModal from '@modals/FullscreenModal';
+import { ArrowLeft } from '@phosphor-icons/react';
+import { IndexableType } from 'dexie';
+import { useContext, useMemo, useState } from 'react';
+import { CategoriesContext, ICategoriesContext } from '../CategoriesContext';
+import CategoryModal from '../category-edit-modal/CategoryModal';
+import CategoryBadge from './category-badge/CategoryBadge';
+import CategoryItem from './CategoryItem';
+import { CategorySelectionFunction } from './CategorySelect';
+import classes from './CategorySelect.module.scss';
 
 type CategorySelectionModalProps = {
-  onClose: () => void
-  categoryIds: IndexableType[]
-  onCategoriesChange: (categories: IndexableType[]) => void
-}
+  onClose: () => void;
+  categoryIds: IndexableType[];
+  onCategoriesChange: (categories: IndexableType[]) => void;
+};
 
 const CategorySelectionModal = ({
   onClose,
   categoryIds,
   onCategoriesChange,
 }: CategorySelectionModalProps) => {
-  const [searchQuery, setSearchQuery] = useState<string>('')
-  const { categories } = useContext<ICategoriesContext>(CategoriesContext)
+  const [searchQuery, setSearchQuery] = useState<string>('');
+  const { categories } = useContext<ICategoriesContext>(CategoriesContext);
+
   const filteredCategories = useMemo(
     () =>
       categories?.filter((c) =>
-        c.name
-          .toLocaleLowerCase()
-          .includes(searchQuery?.toLocaleLowerCase() ?? ''),
+        c.name.toLocaleLowerCase().includes(searchQuery.toLocaleLowerCase() ?? '')
       ),
-    [searchQuery, categories],
-  )
+    [searchQuery, categories]
+  );
 
-  const [isNewCategoryModalOpen, setIsNewCategoryModalOpen] =
-    useState<boolean>(false)
+  const [isNewCategoryModalOpen, setIsNewCategoryModalOpen] = useState<boolean>(false);
 
   const handleOnClick = () => {
-    setIsNewCategoryModalOpen(true)
-  }
+    setIsNewCategoryModalOpen(true);
+  };
 
-  const handleCategorySelection: CategorySelectionFunction = (
-    categoryId,
-    operation,
-  ) => {
+  const handleCategorySelection: CategorySelectionFunction = (categoryId, operation) => {
     switch (operation) {
       case 'add':
-        onCategoriesChange([...categoryIds, categoryId])
-        break
+        onCategoriesChange([...categoryIds, categoryId]);
+        break;
       case 'remove':
-        onCategoriesChange(categoryIds.filter((id) => id !== categoryId))
-        break
+        onCategoriesChange(categoryIds.filter((id) => id !== categoryId));
+        break;
     }
-  }
+  };
 
   return (
     <FullscreenModal>
       <FullscreenModal.Header>
         <ArrowLeft size={24} onClick={onClose} />
-        <FullscreenModal.Title className={classes.modalTitle}>
-          Category Selection
-        </FullscreenModal.Title>
+        <div className={classes.headerContainer}>
+          <FullscreenModal.Title className={classes.modalTitle}>Labels</FullscreenModal.Title>
+          <Button onClick={handleOnClick} className={classes.saveButton}>
+            Save
+          </Button>
+
+          {isNewCategoryModalOpen && (
+            <CategoryModal onClose={() => setIsNewCategoryModalOpen(false)} />
+          )}
+        </div>
       </FullscreenModal.Header>
+
       <div className={classes.modalContent}>
-        <TextField
-          placeholder="Type in what you're looking for"
-          iconKey={'Binoculars'}
-          value={searchQuery}
-          onChange={(event) => setSearchQuery(event.target.value)}
-        />
+        <div className={classes.border}>
+          <TextField
+            placeholder="Search"
+            iconKey={'MagnifyingGlass'}
+            value={searchQuery}
+            size={32}
+            onChange={(event) => setSearchQuery(event.target.value)}
+            className={`${classes.textFieldOverride} ${classes.textFieldWhite}`}
+          />
+        </div>
+        
         <div className={classes.selectionList}>
-          <div className={classes.selectItem}>
-            <div
-              tabIndex={0}
-              className={classes.createNewCategory}
-              onClick={handleOnClick}
-            >
-              Create a new category
-            </div>
-            {isNewCategoryModalOpen && (
-              <CategoryModal onClose={() => setIsNewCategoryModalOpen(false)} />
-            )}
+        <div className={classes.border}>
+
+          {/* Selected Category Badges Section */}
+          <div className={classes.categoryBadges}>
+            <div className={classes.fixedLabel}>Selected</div>
+            {categories
+              ?.filter(({ id }) => categoryIds.includes(id))
+              .map((cat) => (
+                <CategoryBadge
+                  {...cat}
+                  onClick={() => handleCategorySelection(cat.id, 'remove')}
+                  onDelete={() => handleCategorySelection(cat.id, 'remove')}
+                  deletable
+                  key={cat.id.toString()}
+                />
+              ))}
+                </div>
           </div>
-          {filteredCategories
-            ?.filter(({ id }) => !categoryIds.includes(id))
-            .map(({ id, name, color }) => (
-              <CategoryItem
-                name={name}
-                color={color}
-                id={id}
-                key={id.toString()}
-                onChange={handleCategorySelection}
-              />
-            ))}
+          <div className={classes.border}>
+            {/* Selectable Categories Section */}
+            {filteredCategories
+              ?.filter(({ id }) => !categoryIds.includes(id))
+              .map(({ id, name, color }) => (
+                <CategoryItem
+                  name={name}
+                  color={color}
+                  id={id}
+                  key={id.toString()}
+                  onChange={handleCategorySelection}
+                />
+              ))}
+          </div>
         </div>
       </div>
-      <FullscreenModal.ButtonsPanel className={classes.buttons}>
-        <div className={classes.categoryBadges}>
-          {categories
-            ?.filter(({ id }) => categoryIds.includes(id))
-            .map((cat) => (
-              <CategoryBadge
-                {...cat}
-                onClick={() => handleCategorySelection(cat.id, 'remove')}
-                deletable
-                key={cat.id.toString()}
-              />
-            ))}
-        </div>
+      {/* <FullscreenModal.ButtonsPanel className={classes.buttons}>
         <Button variant="secondary" onClick={onClose} underline>
           Go Back
         </Button>
-      </FullscreenModal.ButtonsPanel>
+      </FullscreenModal.ButtonsPanel> */}
     </FullscreenModal>
-  )
-}
+  );
+};
 
-export default CategorySelectionModal
+export default CategorySelectionModal;
